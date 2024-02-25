6e1e3468a96951c10d714d0b1b9369e25b452fb1
Steve Tremblett <steve.tremblett@isara.com>
2024-02-25 13:13:15 -0500

    works sorta

--- keyboards/keychron/q60_max/ansi/keymaps/sjt/keymap.c
+++ keyboards/keychron/q60_max/ansi/keymaps/sjt/keymap.c
@@ -1,21 +1,8 @@
-/* Copyright 2023 @ Keychron (https://www.keychron.com)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/* Copyright 2024 @ sjt */
 
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
+#include "print.h"
 
 enum layers {
     MAC_BASE,
@@ -79,6 +66,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void keyboard_post_init_user(void) {
+    // debug_enable=true;
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
     rgb_matrix_sethsv_noeeprom(HSV_OFF);
 }
